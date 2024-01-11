81ddb43da4fcda009f2f9d47dd3d199901f1e672
Douglas Scaini <douglasscaini.dev@gmail.com>
2024-01-10 21:50:29 -0300

    redux app

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,26 @@
 import ReactPlayer from "react-player";
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
+    const dispath = useDispatch();
+
+    const { currentLesson } = useCurrentLesson();
+
+    function handlePlayNext() {
+        dispath(next());
+    }
+
     return (
         <div className="w-full bg-zinc-950 aspect-video">
-            <ReactPlayer width="100%" height="100%" controls url="https://www.youtube.com/watch?v=564Z4bSJR6w" />
+            <ReactPlayer
+                width="100%"
+                height="100%"
+                controls
+                playing
+                onEnded={handlePlayNext}
+                url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
+            />
         </div>
     );
 }
