0b1f10b81e92bc19d0bde2febae7ba738faf3bac
Douglas Scaini <douglasscaini.dev@gmail.com>
2024-01-11 21:44:05 -0300

    connect api

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,20 +2,28 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
-import { useAppSelector } from "../store";
-import { useCurrentLesson } from "../store/slices/player";
+import { useAppDispatch, useAppSelector } from "../store";
+import { loadCourse, useCurrentLesson } from "../store/slices/player";
 import { useEffect } from "react";
 
 export function Player() {
+    const dispatch = useAppDispatch();
+
     const modules = useAppSelector((state) => {
-        return state.player.course.modules;
+        return state.player.course?.modules;
     });
 
     const { currentLesson } = useCurrentLesson();
 
     useEffect(() => {
-        document.title = `Assistindo ${currentLesson.title}`;
+        if (currentLesson) {
+            document.title = `Assistindo ${currentLesson.title}`;
+        }
     }, [currentLesson]);
+
+    useEffect(() => {
+        dispatch(loadCourse());
+    }, []);
 
     return (
         <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
@@ -35,16 +43,17 @@ export function Player() {
                     </div>
 
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll divide-y-2 divide-zinc-900 scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-                        {modules.map((module, index) => {
-                            return (
-                                <Module
-                                    key={module.id}
-                                    moduleIndex={index}
-                                    title={module.title}
-                                    amountOfLessons={module.lessons.length}
-                                />
-                            );
-                        })}
+                        {modules &&
+                            modules.map((module, index) => {
+                                return (
+                                    <Module
+                                        key={module.id}
+                                        moduleIndex={index}
+                                        title={module.title}
+                                        amountOfLessons={module.lessons.length}
+                                    />
+                                );
+                            })}
                     </aside>
                 </main>
             </div>
