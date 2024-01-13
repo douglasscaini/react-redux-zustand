4b022a8a011793c4bdf9725327a878f35654efb9
Douglas Scaini <douglasscaini.dev@gmail.com>
2024-01-12 21:13:05 -0300

    add zustand

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,8 +1,7 @@
-import { ChevronDown } from "lucide-react";
-import { Lesson } from "./Lesson";
 import * as Collapsible from "@radix-ui/react-collapsible";
-import { useAppDispatch, useAppSelector } from "../store";
-import { play } from "../store/slices/player";
+import { Lesson } from "./Lesson";
+import { useStore } from "../zustand-store";
+import { ChevronDown } from "lucide-react";
 
 interface ModuleProps {
     moduleIndex: number;
@@ -11,16 +10,13 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-    const dispatch = useAppDispatch();
-
-    const { currentModuleIndex, currentLessonIndex } = useAppSelector((state) => {
-        const { currentModuleIndex, currentLessonIndex } = state.player;
-
-        return { currentModuleIndex, currentLessonIndex };
-    });
-
-    const lessons = useAppSelector((state) => {
-        return state.player.course?.modules[moduleIndex].lessons;
+    const { currentModuleIndex, currentLessonIndex, play, lessons } = useStore((store) => {
+        return {
+            lessons: store.course?.modules[moduleIndex].lessons,
+            currentModuleIndex: store.currentModuleIndex,
+            currentLessonIndex: store.currentLessonIndex,
+            play: store.play,
+        };
     });
 
     return (
@@ -50,7 +46,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
                                     title={lesson.title}
                                     duration={lesson.duration}
                                     isCurrent={isCurrent}
-                                    onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                                    onPlay={() => play([moduleIndex, lessonIndex])}
                                 />
                             );
                         })}
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,16 +1,16 @@
-import { MessageCircle } from "lucide-react";
+import { useEffect } from "react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
-import { useAppDispatch, useAppSelector } from "../store";
-import { loadCourse, useCurrentLesson } from "../store/slices/player";
-import { useEffect } from "react";
+import { useCurrentLesson, useStore } from "../zustand-store";
+import { MessageCircle } from "lucide-react";
 
 export function Player() {
-    const dispatch = useAppDispatch();
-
-    const modules = useAppSelector((state) => {
-        return state.player.course?.modules;
+    const { course, load } = useStore((store) => {
+        return {
+            course: store.course,
+            load: store.load,
+        };
     });
 
     const { currentLesson } = useCurrentLesson();
@@ -22,7 +22,7 @@ export function Player() {
     }, [currentLesson]);
 
     useEffect(() => {
-        dispatch(loadCourse());
+        load();
     }, []);
 
     return (
@@ -43,8 +43,8 @@ export function Player() {
                     </div>
 
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll divide-y-2 divide-zinc-900 scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-                        {modules &&
-                            modules.map((module, index) => {
+                        {course?.modules &&
+                            course?.modules.map((module, index) => {
                                 return (
                                     <Module
                                         key={module.id}
